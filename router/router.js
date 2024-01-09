5a57982a7e09d5e48f505366ca4c41d1c4bf4df2
Océanc <oceance@MacBook-Air-de-Oceanc.local>
2024-01-09 10:38:03 +0100

    Update router

--- router/router.js
+++ router/router.js
@@ -1,6 +1,5 @@
 // Import controller
 const MainController = require('../controllers/MainController');
-const HomeController = require('../controllers/HomeController');
 const errorController = require('../controllers/ErrorController');
 const notFoundController = require('../controllers/NotFoundController');
 const forbiddenController = require('../controllers/ForbiddenController');
@@ -14,7 +13,7 @@ module.exports = (app) => {
         router.get("/sensor", MainController.sensor);
         // Page error
         router.get("/errors/forbidden", errorHandler, forbiddenController.renderForbiddenPage);
-        router.get("/errors/not-found", ergirorHandler, notFoundController.renderNotFoundPage);
+        router.get("/errors/not-found", errorHandler, notFoundController.renderNotFoundPage);
         router.get("/errors/error-internal", errorHandler, errorController.renderErrorPage);
         router.get("/errors/bad-request", errorHandler, badRequestController.renderBadRequestPage);
         // router.post('/create', ExampleController.create); // Create
