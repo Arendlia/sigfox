37a18d0cfef1d78a133a4fa53774f2e0e956c6dc
Océanc <oceance@MacBook-Air-de-Oceanc.local>
2024-01-11 09:10:25 +0100

    Update

--- controllers/ForbiddenController.js
+++ controllers/ForbiddenController.js
@@ -1,6 +1,8 @@
 // controllers/forbiddenController.js
+const test = require('../Function/convertHexaTojson');
 const renderForbiddenPage = (req, res) => {
     res.status(403).render('errors/forbidden');
+    
 };
 
 module.exports = {
--- controllers/MainController.js
+++ controllers/MainController.js
@@ -4,18 +4,11 @@ exports.home = async (req, res) => {
     return res.render('search');
 }
 
-exports.homePost = async (req, res) => {
-    console.log(req);
-    const id = req.body.id;
-    res.redirect('/sensor/'+id);
-}
-
 exports.sensor = async (req, res) => {
-    console.log('top')
     try {
         let result = await axios({
             'method': 'GET',
-            'url': process.env.API_URL+'/devices/'+req.params.id,
+            'url': process.env.API_URL+'/devices/'+req.query.id,
             'headers': 
             {
                 'Content-Type': 'application/json'
@@ -25,52 +18,30 @@ exports.sensor = async (req, res) => {
                 'password': process.env.API_PASSWORD
             }
         });
+        let sensor = result.data
+        if (sensor.group.id == process.env.API_SENSOR_GROUP) {
+            result =  await axios({
+                'method': 'GET',
+                'url': process.env.API_URL+'/devices/'+req.query.id+'/messages',
+                'headers': 
+                {
+                    'Content-Type': 'application/json'
+                },
+                'auth': {
+                    'username': process.env.API_USERNAME,
+                    'password': process.env.API_PASSWORD
+                }
+            });
+        }
+
+        
+
         return res.render('sensor', {'sensor': result.data});
     } catch (e) {
         
     }
 }
 
-exports.getFirstMessage = async (req, res) => {
-    let url = process.env.API_URL+'/devices/'+req.params.id+'/messages?limit1';
-    let results = await axios({
-        'method': 'GET',
-        'url': url,
-        'headers': 
-        {
-            'Content-Type': 'application/json'
-        },
-        'auth': {
-            'username': process.env.API_USERNAME,
-            'password': process.env.API_PASSWORD
-        }
-    });
-    return res.send(results.data)
-}
-
 exports.sensorMessagesAjax = async (req, res) => {
-    let results = [];
-    let url = process.env.API_URL+'/devices/'+req.params.id+'/messages';
-    let result = {};
-    while (url) {
-        if (result != {}) {
-            await new Promise(r => setTimeout(r, 1000));
-        }
-        result = await axios({
-            'method': 'GET',
-            'url': url,
-            'headers': 
-            {
-                'Content-Type': 'application/json'
-            },
-            'auth': {
-                'username': process.env.API_USERNAME,
-                'password': process.env.API_PASSWORD
-            }
-        });
-        console.log(result.status);
-        results.push(result?.data?.data);
-        url = result?.data?.paging?.next ?? null;
-    }
-    return res.send(results);
+
 }
