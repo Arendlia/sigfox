ff0501607a5155a9d155454986d92650f2a48fe1
Cécilia <cecilia.ruin@gmail.com>
2024-01-12 08:48:25 +0100

    add another two cards

--- public/js/daterange.js
+++ public/js/daterange.js
@@ -19,6 +19,7 @@ $(function() {
     } );
     function cb(start, nowDate) {
         $('#daterange span').html(start.toLocaleDateString() + ' - ' + nowDate.toLocaleDateString());
+        $('#daterange-mobile span').html(start.toLocaleDateString() + ' - ' + nowDate.toLocaleDateString());
     }
     cb(startDate, nowDate)
     $('#daterange').daterangepicker({
@@ -33,4 +34,26 @@ $(function() {
         $('#daterange span').html(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'));
       console.log("A new date selection was made: " + start.format('DD/MM/YYYY') + ' to ' + end.format('DD/MM/YYYY'));
     });
+
+
+
+
+    $('#daterange-mobile').daterangepicker({
+      startDate: startDate,
+      opens: 'left',
+      minDate: new Date(nowDate.getFullYear(), nowDate.getMonth()-1, nowDate.getDate(), 0, 0, 0, 0),
+      maxDate: new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 0, 0, 0, 0),
+      locale: {
+        format: 'DD/MM/YYYY'
+      },
+      ranges: {
+         '1 jour': [moment(), moment()],
+         '1 semaine': [moment().subtract(7, 'days'), moment()],
+         '1 mois': [moment().subtract(1, 'mounth'), moment()],
+      }
+  }, cbMobile);
+  function cbMobile(start, end) {
+    $('#daterange-mobile span').html(start.format('DD/MM/YYYY') + ' - ' + end.format('DD/MM/YYYY'));
+  }
+  $('[data-range-key="Custom Range"]').text('Choisir sa durée');
   })
