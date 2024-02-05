693f1d7cc55b9f1acd9ec62b44df3ac431a5007d
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2024-02-05 10:11:20 +0100

    Affichage livedata

--- public/js/bornes.js
+++ public/js/bornes.js
@@ -1,3 +1,6 @@
+
+
+
 const humidityBornes = {
     "minDanger": 10,
     "minWarning": 30,
@@ -17,63 +20,66 @@ const batteryBornes = {
     "success": 70,
 }
 
-$(function() {
+function humidity(){
     var humidityValue = $('#humidityData').text()
+    if(humidityValue > humidityBornes["minDanger"] && humidityValue < humidityBornes["minWarning"]){
+        $('.fa-droplet').addClass('text-warning')
+    }
+    if(humidityValue <= humidityBornes["minDanger"] || humidityValue >= humidityBornes["maxDanger"]){
+        $('.fa-droplet').addClass('text-danger')
+    }
+    if(humidityValue < humidityBornes["maxDanger"] && humidityValue > humidityBornes["maxWarning"]){
+        $('.fa-droplet').addClass('text-warning')
+    }
+    if(humidityValue >= humidityBornes["minWarning"] && humidityValue <= humidityBornes["maxWarning"]){
+        $('.fa-droplet').addClass('text-success')
+    }
+}
+function temperature(){
     var temperatureValue = $('#temperatureData').text()
+    if(temperatureValue > temperatureBornes["minDanger"] && temperatureValue < temperatureBornes["minWarning"]){
+        $('.fa-temperature-three-quarters').addClass('text-warning')
+    }
+    if(temperatureValue <= temperatureBornes["minDanger"] || temperatureValue >= temperatureBornes["maxDanger"]){
+        $('.fa-temperature-three-quarters').addClass('text-danger')
+    }
+    if(temperatureValue < temperatureBornes["maxDanger"] && temperatureValue > temperatureBornes["maxWarning"]){
+        $('.fa-temperature-three-quarters').addClass('text-warning')
+    }
+    if(temperatureValue >= temperatureBornes["minWarning"] && temperatureValue <= temperatureBornes["maxWarning"]){
+        $('.fa-temperature-three-quarters').addClass('text-success')
+    }
+}
+function battery(){
     var batteryValue = $('#batteryData').text()
+    if(batteryValue <= batteryBornes["danger"]){
+        $('.fa-battery-three-quarters').addClass('text-danger')
+    }
+    if(batteryValue > batteryBornes["danger"] && batteryValue <= batteryBornes["success"]){
+        $('.fa-battery-three-quarters').addClass('text-warning')
+    }
+    if(batteryValue >= batteryBornes["success"]){
+        $('.fa-battery-three-quarters').addClass('text-success')
+    }
+}
+function connexion(){
     var dataValue = $('#dataWifi').text()
-    function humidity(){
-        if(humidityValue > humidityBornes["minDanger"] && humidityValue < humidityBornes["minWarning"]){
-            $('.fa-droplet').addClass('text-warning')
-        }
-        if(humidityValue <= humidityBornes["minDanger"] || humidityValue >= humidityBornes["maxDanger"]){
-            $('.fa-droplet').addClass('text-danger')
-        }
-        if(humidityValue < humidityBornes["maxDanger"] && humidityValue > humidityBornes["maxWarning"]){
-            $('.fa-droplet').addClass('text-warning')
-        }
-        if(humidityValue >= humidityBornes["minWarning"] && humidityValue <= humidityBornes["maxWarning"]){
-            $('.fa-droplet').addClass('text-success')
-        }
+    if(dataValue == "Limitée" ){
+        $('.fa-wifi').addClass('text-danger')
     }
-    function temperature(){
-        if(temperatureValue > temperatureBornes["minDanger"] && temperatureValue < temperatureBornes["minWarning"]){
-            $('.fa-temperature-three-quarters').addClass('text-warning')
-        }
-        if(temperatureValue <= temperatureBornes["minDanger"] || temperatureValue >= temperatureBornes["maxDanger"]){
-            $('.fa-temperature-three-quarters').addClass('text-danger')
-        }
-        if(temperatureValue < temperatureBornes["maxDanger"] && temperatureValue > temperatureBornes["maxWarning"]){
-            $('.fa-temperature-three-quarters').addClass('text-warning')
-        }
-        if(temperatureValue >= temperatureBornes["minWarning"] && temperatureValue <= temperatureBornes["maxWarning"]){
-            $('.fa-temperature-three-quarters').addClass('text-success')
-        }
+    if(dataValue == "Bonne"){
+        $('.fa-wifi').addClass('text-warning')
     }
-    function battery(){
-        if(batteryValue <= batteryBornes["danger"]){
-            $('.fa-battery-three-quarters').addClass('text-danger')
-        }
-        if(batteryValue > batteryBornes["danger"] && batteryValue <= batteryBornes["success"]){
-            $('.fa-battery-three-quarters').addClass('text-warning')
-        }
-        if(batteryValue >= batteryBornes["success"]){
-            $('.fa-battery-three-quarters').addClass('text-success')
-        }
+    if(dataValue == "Très bonne"){
+        $('.fa-wifi').addClass('text-success')
     }
-    function connexion(){
-        if(dataValue == "Limitée" ){
-            $('.fa-wifi').addClass('text-danger')
-        }
-        if(dataValue == "Bonne"){
-            $('.fa-wifi').addClass('text-warning')
-        }
-        if(dataValue == "Très bonne"){
-            $('.fa-wifi').addClass('text-success')
-        }
-    }
+}
+
+function setColors() {
     humidity()
     temperature()
     battery()
     connexion()
-});
+    $('#liveDataLoader').addClass('d-none');
+    $('#liveData').removeClass('invisible');
+}
