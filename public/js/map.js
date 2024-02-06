9bfdadd2b004832da10f87f5b18036e5615a8ecd
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2024-02-06 10:14:32 +0100

    fix leaflet execution conditionnelle

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,5 @@
 $(function () {
-    if ($('#map') == null) {
+    if ($('#map').length != 0) {
         var map = L.map('map').setView([51.505, -0.09], 13);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
