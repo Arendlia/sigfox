39de408518b20dca38ebc5d970df00b713871c30
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2024-02-06 16:29:00 +0100

    fix decodage bswarm

--- function/bSwarmUncoding.js
+++ function/bSwarmUncoding.js
@@ -1,34 +1,70 @@
 function bSwarmUncoding(timestamp, value) {
-    let lat = parseInt(value.slice(0, 8), 16).toString(2).padStart(4, '0');
-    let lon = parseInt(value.slice(8, 16), 16).toString(2).padStart(4, '0');
-    let temperature = parseInt(value.slice(16, 18), 16).toString(2).padStart(4, '0');
+    let latBinary = 0;
+    let lngBinary = 0;
+    let temperatureBinary = 0;
+    let uncoded = {};
 
-    let latInt = parseInt(lat, 2);
-    let lonInt = parseInt(lon, 2);
-    let temperatureInt = parseInt(temperature, 2);
+    if (value.length != 2) {
+        latBinary = parseInt(value.slice(0, 8), 16).toString(2).padStart(32, '0');
+        lngBinary = parseInt(value.slice(8, 16), 16).toString(2).padStart(32, '0');
+        temperatureBinary = parseInt(value.slice(16, 18), 16).toString(2).padStart(8, '0');
+        uncoded = {
+            latitude: decodeLatitude(latBinary),
+            longitude: decodeLongitude(lngBinary),
+            temperature: decodeTemperature(temperatureBinary),
+            date: timestamp
+        }
+    } else {
+        temperatureBinary = parseInt(value).toString(2).padStart(8, '0');
+        uncoded = {
+            temperature: decodeTemperature(temperatureBinary),
+            date: timestamp
+        }
+    }
 
-    // Latitude decoding
-    let latSign = (latInt & 0x80000000) ? -1 : 1;
-    let latValue = latSign * (latInt & 0x7FFFFFFF);
-    let latDegrees = Math.floor(latValue / 1000000);
-    let latMinutes = ((latValue % 1000000) / 60) * 100;
+    return uncoded;
+}
 
-    // Longitude decoding
-    let lngSign = (lonInt & 0x80000000) ? -1 : 1;
-    let lngValue = lngSign * (lonInt & 0x7FFFFFFF);
-    let lngDegrees = Math.floor(lngValue / 1000000);
-    let lngMinutes = ((lngValue % 1000000) / 60) * 100;
+// Latitude decoding
+function decodeLatitude(binaryLatitude) {
+    let latsliced = binaryLatitude.substring(1);
+    let latInt = parseInt(latsliced, 2);
+    let latDegrees = Math.floor(latInt / 1000000);
+    let latMinutes = (((latInt / 1000000) % 1) / 60) * 100;
+    let latitude = 0;
+    if (binaryLatitude[0] == '1') {
+        latDegrees = latDegrees * -1;
+        latitude = latDegrees - latMinutes;
+    } else {
+        latitude = latDegrees + latMinutes;
+    }
+    return latitude.toFixed(5);
+}
 
-    // Temperature decoding
-    let tempSign = (temperatureInt & 0x80000000) ? -1 : 1;
-    let tempValue = tempSign * (temperatureInt & 0x7FFFFFFF);
-
-    return {
-        date: timestamp,
-        latitude: latDegrees + "." + latMinutes,
-        longitude: lngDegrees + "." + lngMinutes,
-        temperature: tempValue
+// Longitude decoding
+function decodeLongitude(binaryLongitude) {
+    let lngsliced = binaryLongitude.substring(1);
+    let lngInt = parseInt(lngsliced, 2);
+    let lngDegrees = Math.floor(lngInt / 1000000);
+    let lngMinutes = (((lngInt / 1000000) % 1) / 60) * 100;
+    let longitude = 0;
+    if (binaryLongitude[0] == '1') {
+        lngDegrees = lngDegrees * -1;
+        longitude = lngDegrees - lngMinutes;
+    } else {
+        longitude = lngDegrees + lngMinutes;
     };
+    return longitude.toFixed(5);
+}
+
+// Temperature decoding
+function decodeTemperature(binaryTemperature) {
+    let temperatureSliced = binaryTemperature.substring(1);
+    let temperature = parseInt(temperatureSliced, 2);
+    if (binaryTemperature[0] == '1') {
+        temperature = temperature * -1;
+    }
+    return temperature
 }
 
 module.exports = bSwarmUncoding;
