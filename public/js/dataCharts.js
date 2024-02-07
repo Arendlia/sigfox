05be6aadb2c6268fb99810b3ace7a733f6d19d3b
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2024-02-07 14:29:49 +0100

    pas de chargement graphique si pas de données

--- public/js/dataCharts.js
+++ public/js/dataCharts.js
@@ -25,50 +25,54 @@ $(function() {
 		'method': 'GET',
 		'url': '/sensor/'+$('#sensorId').data('id')+'/messages?device-type='+$('#sensorId').data('sensorGroup')
 	}).then(function(result) {
-		console.log(result);
-		setMap(result.data);
-		setLastPositions(result.data);
-		datareversed = result.data.reverse();
-		if ($('#sensorId').data('sensorGroup') == '643d74c4e0b8bb55977b2e59') {
-			$('#batteryData').text(result.data[0].battery);
-			$('#humidityData').text(result.data[0].humidity);
-			$('#humidityloading').removeClass('d-flex');
-			$('#humidityloading').addClass('d-none');
-			$('#humiditychart').removeClass('d-none');
-			humidityseries = createChart('humiditychart', datareversed, 'humidity', '%');
-		} else if ($('#sensorId').data('sensorGroup') == '6445409bc69c3c4137aea1c7') {
-			$('#latitudeData').text(result.data[0].latitude);
-			$('#longitudeData').text(result.data[0].longitude);
-		}
-		$('#temperatureData').text(result.data[0].temperature);
-		$('#temperatureloading').removeClass('d-flex');
-		$('#temperatureloading').addClass('d-none');
-		$('#temperaturechart').removeClass('d-none');
-		setColors();
-		temperatureseries = createChart('temperaturechart',	datareversed, 'temperature', '°C');
-		$('.periodbutton').each(function() {
-			$(this).on('click', function() {
-				let date = new Date();
-				filteredData = filterDataByDate(datareversed, date.setDate(date.getDate() - this.dataset.period), new Date());
-				temperatureseries.data.setAll(filteredData);
-				humidityseries.data.setAll(filteredData);
+		if (result) {
+			setMap(result.data);
+			setLastPositions(result.data);
+			datareversed = result.data.reverse();
+			if ($('#sensorId').data('sensorGroup') == '643d74c4e0b8bb55977b2e59') {
+				$('#batteryData').text(result.data[0].battery);
+				$('#humidityData').text(result.data[0].humidity);
+				$('#humidityloading').removeClass('d-flex');
+				$('#humidityloading').addClass('d-none');
+				$('#humiditychart').removeClass('d-none');
+				humidityseries = createChart('humiditychart', datareversed, 'humidity', '%');
+			} else if ($('#sensorId').data('sensorGroup') == '6445409bc69c3c4137aea1c7') {
+				$('#latitudeData').text(result.data[0].latitude);
+				$('#longitudeData').text(result.data[0].longitude);
+				$('#map').removeClass('d-none');
+				$('#mapLoader').addClass('d-none');
+			}
+			$('#temperatureData').text(result.data[0].temperature);
+			$('#temperatureloading').removeClass('d-flex');
+			$('#temperatureloading').addClass('d-none');
+			$('#temperaturechart').removeClass('d-none');
+			temperatureseries = createChart('temperaturechart',	datareversed, 'temperature', '°C');
+			setColors();
+			
+			$('.periodbutton').each(function() {
+				$(this).on('click', function() {
+					let date = new Date();
+					filteredData = filterDataByDate(datareversed, date.setDate(date.getDate() - this.dataset.period), new Date());
+					temperatureseries.data.setAll(filteredData);
+					humidityseries.data.setAll(filteredData);
+				})
 			})
-		})
-
-		$('#daterange').on('apply.daterangepicker', function(ev, picker) {
-			filteredData = filterDataByDate(datareversed, picker.startDate.toDate().getTime(), picker.endDate.toDate().getTime());
-			temperatureseries.data.setAll(filteredData);
-			humidityseries.data.setAll(filteredData)
-		})
-
-		$('#daterange-mobile').on('apply.daterangepicker', function(ev, picker) {
-			filteredData = filterDataByDate(datareversed, picker.startDate.toDate().getTime(), picker.endDate.toDate().getTime());
-			temperatureseries.data.setAll(filteredData);
-			humidityseries.data.setAll(filteredData)
-		})
-
-		$('#collapse').on('click', '.sensor-position-item', function() {
-			setMarker($(this).data('lat'), $(this).data('lng'))
-		})
+	
+			$('#daterange').on('apply.daterangepicker', function(ev, picker) {
+				filteredData = filterDataByDate(datareversed, picker.startDate.toDate().getTime(), picker.endDate.toDate().getTime());
+				temperatureseries.data.setAll(filteredData);
+				humidityseries.data.setAll(filteredData)
+			})
+	
+			$('#daterange-mobile').on('apply.daterangepicker', function(ev, picker) {
+				filteredData = filterDataByDate(datareversed, picker.startDate.toDate().getTime(), picker.endDate.toDate().getTime());
+				temperatureseries.data.setAll(filteredData);
+				humidityseries.data.setAll(filteredData)
+			})
+	
+			$('#collapse').on('click', '.sensor-position-item', function() {
+				setMarker($(this).data('lat'), $(this).data('lng'))
+			})
+		}
 	});
 })
