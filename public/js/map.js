8da98f0cf44ae82e63cf19b29f5224550a27e753
guillaume-bgr <guillaumebongrand.lamanu@gmail.com>
2024-02-07 11:13:27 +0100

    passage map en fonction

--- public/js/map.js
+++ public/js/map.js
@@ -1,25 +1,17 @@
-$(function () {
+var marker = undefined;
+var map = L.map('map');
+
+function setMap(data) {
     if ($('#map').length != 0) {
-        var map = L.map('map').setView([51.505, -0.09], 13);
+        map.setView([data[0].latitude, data[0].longitude], 13);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
         }).addTo(map);
-        var icon = L.icon({
-            iconUrl: '/images/dot.svg',
-
-            iconSize: [38, 95], // size of the icon
-            shadowSize: [50, 64], // size of the shadow
-            iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-            shadowAnchor: [4, 62], // the same for the shadow
-            popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-        });
-        L.marker([51.5, -0.09], {
-            icon: icon
-        }).addTo(map);
+        marker = setMarker(data[0]);
         var legend = L.control({
             position: 'topright'
         });
-        legend.onAdd = function (map) {
+        legend.onAdd = function () {
             var button = L.DomUtil.create("button", "btn btn-light");
             button.setAttribute('id', 'legentBtn')
             button.setAttribute('type', 'button');
@@ -47,4 +39,21 @@ $(function () {
 
         });
     }
-})
+
+    return map;
+}
+
+function setMarker(data) {
+    let icon = L.icon({
+        iconUrl: '/images/dot.svg',
+        iconSize: [38, 95], // size of the icon
+        shadowSize: [50, 64], // size of the shadow
+        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62], // the same for the shadow
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    return L.marker([data.latitude, data.longitude], {
+        icon: icon
+    }).addTo(map);
+}
